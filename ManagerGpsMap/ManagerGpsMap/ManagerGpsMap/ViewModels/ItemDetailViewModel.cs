8a940f35553a71e58d9cec5096da6bbed435302f
luulong83 <luucangkimlong83@yahoo.com.vn>
2018-03-08 09:28:46 +0700

    create project
    Tao project

---
+++ ManagerGpsMap/ManagerGpsMap/ManagerGpsMap/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ManagerGpsMap.Models;
+
+namespace ManagerGpsMap.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ ManagerGpsMap/ManagerGpsMap/ManagerGpsMap/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ManagerGpsMap.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
